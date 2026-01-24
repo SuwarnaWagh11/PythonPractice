9b87f309f6ee0a55bdb1e9a6e92f0daa3905a5f6
Suwarna_Wagh <2zo756s3g6av2j435b5zvaf26xp724eshz7svp5ae26xclxdcyva>
2026-01-24 12:40:34 +0100

    testing print

--- DailyPractice/2026/January/Jan24.py
+++ DailyPractice/2026/January/Jan24.py
@@ -1,1 +1,5 @@
 print("My Name is Suwarna, I am from India and My age is 34")
+age = 34
+name = 'Suwarna Wagh'
+country = 'India'
+print('My name is ', name, ', my age is ', age, 'I love my ', country)
