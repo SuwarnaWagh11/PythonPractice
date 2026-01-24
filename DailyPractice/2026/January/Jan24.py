ae3d40d7da00eb7be2096a4dddf4c92cfa6ba5ad
Suwarna_Wagh <suwarnawagh11@gmail.com>
2026-01-24 12:52:08 +0100

    Comment - # and '''

--- DailyPractice/2026/January/Jan24.py
+++ DailyPractice/2026/January/Jan24.py
@@ -1,5 +1,8 @@
+'''
+this is the block of comment, it is another way of making comment apart from #.
+'''
 print("My Name is Suwarna, I am from India and My age is 34")
-age = 34
-name = 'Suwarna Wagh'
+age = 34  # numbers
+name = 'Suwarna Wagh'  # string
 country = 'India'
 print('My name is ', name, ', my age is ', age, 'I love my ', country)
